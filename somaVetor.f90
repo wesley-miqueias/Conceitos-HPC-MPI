419202502b11ec7430cf63843a3871d70caa6e00
wesley-miqueias <wesleymiqueias21@gmail.com>
2025-12-10 08:16:32 -0300

    Aplicando NT

--- somaVetor.f90
+++ somaVetor.f90
@@ -1,21 +1,23 @@
 program somaVetor
     implicit none
 
-    integer, parameter :: N = 100000000
+    integer, parameter :: N = 100000000, NT=1
     real(8) :: v(N)
     real(8) :: soma
-    integer :: i
+    integer :: i,j
 
     
     do i = 1, N
     v(i) = 1.0d0
     end do
 
-    
-    soma = 0.0d0
-    do i = 1, N
-    soma = soma + v(i)
-    end do
+    do j=1, NT
+   
+        soma = 0.0d0
+        do i = 1, N
+            soma = soma + v(i)
+        end do
+    end do    
 
 
     print *, "Soma =", soma
