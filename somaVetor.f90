10d2170eb9abe728faaa494fb15b919cef636144
Wesley Miqueias <wesley@DESKTOP-A2GCURI.localdomain>
2025-12-14 12:41:47 -0300

    Atualiza projeto e adiciona .gitignore

---
+++ somaVetor.f90
@@ -0,0 +1,28 @@
+program somaVetor
+   implicit none
+
+   integer, parameter  :: N = 100000000, NT=1
+   real                :: v(N)
+   real(8)             :: soma
+   integer             :: i,j
+
+   
+   do j=1, NT
+     
+      do i = 1, N
+         v(i) = 1.0
+      end do
+
+   
+      soma = 0.0
+     
+      do i = 1, N
+         soma = soma + v(i)
+      end do
+   
+   end do ! NT
+
+   print*, "N     = ", N
+   print*, "Soma  = ", soma
+
+end program somaVetor
