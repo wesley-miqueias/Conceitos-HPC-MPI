e67555a96e198d56ca6a26950c0cca5c094782f7
wesley-miqueias <wesleymiqueias21@gmail.com>
2025-12-16 09:46:55 -0300

    Corrige e atualiza somaVetor.f90

--- somaVetor.f90
+++ somaVetor.f90
@@ -1,8 +1,7 @@
 program somaVetor
-<<<<<<< HEAD
    implicit none
 
-   integer, parameter  :: N = 100000000, NT=1
+   integer, parameter  :: N = 100000000, NT=
    real                :: v(N)
    real(8)             :: soma
    integer             :: i,j
@@ -27,29 +26,3 @@ program somaVetor
    print*, "Soma  = ", soma
 
 end program somaVetor
-=======
-    implicit none
-
-    integer, parameter :: N = 100000000, NT=1
-    real(8) :: v(N)
-    real(8) :: soma
-    integer :: i,j
-
-    
-    do i = 1, N
-    v(i) = 1.0d0
-    end do
-
-    do j=1, NT
-   
-        soma = 0.0d0
-        do i = 1, N
-            soma = soma + v(i)
-        end do
-    end do    
-
-
-    print *, "Soma =", soma
-
-end program somaVetor
->>>>>>> 419202502b11ec7430cf63843a3871d70caa6e00
