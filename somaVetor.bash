438580f33995405b7485cf3a5d3e4c16daaaeeff
wesley-miqueias <wesleymiqueias21@gmail.com>
2025-12-16 07:59:32 -0300

    Add arquivo bash

---
+++ somaVetor.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+SOURCE="somaVetor.bas"
+OPTS=("-O0" "-O1" "-O2" "-O3")
+
+echo "--- Compilação e Teste (O0 a O3) ---"
+for OPT in "${OPTS[@]}"; do
+    OUTPUT="somaVetor_${OPT//-}"; 
+    echo -n "Compilando $OPT... "; gfortran $OPT $SOURCE -o $OUTPUT && echo " OK" || echo " FAIL"
+    [ -x "$OUTPUT" ] && (echo "--- Tempo para $OPT ---"; time ./"$OUTPUT"; echo "----------------------")
+done
