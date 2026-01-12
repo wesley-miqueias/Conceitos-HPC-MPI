eceb460ab0f89cecdcb13909bb3874a20eb660ee
wesley-miqueias <wesleymiqueias21@gmail.com>
2026-01-12 11:21:52 -0300

    Adiciona exemplos MPI Hello World

---
+++ hello_world_MPI.f90
@@ -0,0 +1,11 @@
+program hello_mpi
+    use mpi
+    implicit none
+    integer :: ierr
+
+    call MPI_Init(ierr)
+
+    print *, "Hello World"
+
+    call MPI_Finalize(ierr)
+end program hello_mpi
