f10047d05f7083ddbe1cd199ede3283f935ee6c3
wesley-miqueias <wesleymiqueias21@gmail.com>
2026-02-10 08:39:52 -0300

    Atualização do codigo MPI mestre-escravo

--- somaVetor_mpi_mestre_escravo.f90
+++ somaVetor_mpi_mestre_escravo.f90
@@ -5,29 +5,35 @@ program somaVetor_mpi_mestre_escravo
     integer, parameter :: N = 1000000
     real(8), allocatable :: v(:)
     real(8) :: soma_local, soma_total
+    real(8) :: t0, t1, tempo_local, tempo_max
     integer :: rank, nprocs, ierr, chunk, i
     integer :: status(MPI_STATUS_SIZE)
-    real(8) :: t0, t1
 
     call MPI_Init(ierr)
     call MPI_Comm_rank(MPI_COMM_WORLD, rank, ierr)
     call MPI_Comm_size(MPI_COMM_WORLD, nprocs, ierr)
 
-    if (rank == 0) then
-        t0 = MPI_Wtime()
+    chunk = N / nprocs
 
-        chunk = N / nprocs
+    call MPI_Barrier(MPI_COMM_WORLD, ierr)
+    t0 = MPI_Wtime()
+
+    if (rank == 0) then
+
         soma_total = 0.0
 
+        ! Envia o tamanho do trabalho
         do i = 1, nprocs-1
             call MPI_Send(chunk, 1, MPI_INTEGER, i, 0, MPI_COMM_WORLD, ierr)
         end do
 
         allocate(v(chunk))
-        v = real(rank + 1, 8)
-        soma_total = sum(v)
+        v = 1.0_8
+        soma_local = sum(v)
+        soma_total = soma_local
         deallocate(v)
 
+        ! Recebe as somas
         do i = 1, nprocs-1
             call MPI_Recv(soma_local, 1, MPI_DOUBLE_PRECISION, i, 1, MPI_COMM_WORLD, status, ierr)
             soma_total = soma_total + soma_local
@@ -35,18 +41,43 @@ program somaVetor_mpi_mestre_escravo
 
         t1 = MPI_Wtime()
 
-        print *, "Processos =", nprocs
-        print *, "Soma =", soma_total
-        print *, "Tempo =", t1 - t0
-
     else
+
         call MPI_Recv(chunk, 1, MPI_INTEGER, 0, 0, MPI_COMM_WORLD, status, ierr)
+
         allocate(v(chunk))
-        v = real(rank + 1, 8)
+        v = 1.0_8
         soma_local = sum(v)
-        call MPI_Send(soma_local, 1, MPI_DOUBLE_PRECISION, 0, 1, MPI_COMM_WORLD, ierr)
         deallocate(v)
+
+        call MPI_Send(soma_local, 1, MPI_DOUBLE_PRECISION, 0, 1, MPI_COMM_WORLD, ierr)
+
+    end if
+
+    tempo_local = MPI_Wtime() - t0
+
+    ! pega o MAIOR tempo (o que define o desempenho real)
+    call MPI_Reduce(tempo_local, tempo_max, 1, MPI_DOUBLE_PRECISION, MPI_MAX, 0, MPI_COMM_WORLD, ierr)
+
+    call MPI_Barrier(MPI_COMM_WORLD, ierr)
+
+    ! imprime em ordem
+    do i = 0, nprocs-1
+        if (rank == i) then
+            write(*,'(A,I2,A,F10.6,A)') "Rank ", rank, " -> Tempo = ", tempo_local, " s"
+        end if
+        call MPI_Barrier(MPI_COMM_WORLD, ierr)
+    end do
+
+    if (rank == 0) then
+        print *
+        print *, "============================"
+        write(*,'(A,I2)') "Processos = ", nprocs
+        write(*,'(A,F12.0)') "Soma correta = ", soma_total
+        write(*,'(A,F10.6,A)') "Tempo TOTAL (max) = ", tempo_max, " s"
+        print *, "============================"
     end if
 
     call MPI_Finalize(ierr)
+
 end program somaVetor_mpi_mestre_escravo
